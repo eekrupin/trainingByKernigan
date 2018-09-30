c7084c809437619052be3e894b4cb18df950bdf8
eekrupin <eekrupin@gmail.com>
2018-09-30 15:09:51 +0300

    refactor

--- hw3/ch5_findImgLinks/main.go
+++ hw3/ch5_findImgLinks/main.go
@@ -7,55 +7,53 @@ import (
 	"os"
 )
 
-func main() {
-
-	url := os.Args[1] //"https://goods.ru/"
-	resp, err := getResponse(url)
-	defer resp.Body.Close()
-
-	doc := getDoc(err, resp)
-	links := getLinksMap(doc)
-
-	for key := range links {
-		fmt.Println(key)
-	}
-
-}
-
-func getLinksMap(doc *html.Node) map[string]string {
-	links := make(map[string]string)
-	visit(links, doc)
-	return links
-}
-
-func getDoc(err error, resp *http.Response) *html.Node {
-	doc, err := html.Parse(resp.Body)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "findImgLinks: %v\n", err)
-		os.Exit(1)
-	}
-	return doc
-}
-
 func getResponse(url string) (*http.Response, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		fmt.Fprintf(os.Stderr, "get url: %v\n", err)
 		os.Exit(1)
 	}
 	return resp, err
 }
 
-func visit(links map[string]string, node *html.Node) {
-	if node.Type == html.ElementNode && node.Data == "img" {
+func getDoc(err error, resp *http.Response) *html.Node {
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "get doc: %v\n", err)
+		os.Exit(1)
+	}
+	return doc
+}
+
+func visit(links map[string]string, node *html.Node, nodeStart, attrKey string) {
+	if node.Type == html.ElementNode && node.Data == nodeStart {
 		for _, a := range node.Attr {
-			if a.Key == "src" {
+			if a.Key == attrKey {
 				links[a.Val] = ""
 			}
 		}
 	}
 
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		visit(links, c)
+		visit(links, c, nodeStart, attrKey)
+	}
+}
+
+func getLinksMap(doc *html.Node, nodeStart, attrKey string) map[string]string {
+	links := make(map[string]string)
+	visit(links, doc, nodeStart, attrKey)
+	return links
+}
+
+func main() {
+	url := os.Args[1] //"https://goods.ru/"
+	resp, err := getResponse(url)
+	defer resp.Body.Close()
+
+	doc := getDoc(err, resp)
+	links := getLinksMap(doc, "img", "src")
+
+	for key := range links {
+		fmt.Println(key)
 	}
 }
